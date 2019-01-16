387d3b7b2b45fc2d4e22d3deb0b033a5d43475e1
Admin <admin@MAC11.local>
2019-01-16 14:54:42 +0700

    Initial Commit

---
+++ HW1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by Admin on 16/1/2562 BE.
+//  Copyright © 2562 th.ac.kmutnb.www. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
